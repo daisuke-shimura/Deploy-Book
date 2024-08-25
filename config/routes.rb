5ea45211c97b55edcff17134cd765889519cac3f
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-08-25 16:34:21 +0000

    ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
+  root to: "homes#top"
+  get 'about' => "homes#about"
   devise_for :users
+
+  resources :users, only: [:index,:show,:edit]
+  resources :books, only: [:new,:index,:show,:edit]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
