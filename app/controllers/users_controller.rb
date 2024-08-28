40d20c1b34a2cb94b15172041057e11113c62696
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-08-28 00:57:46 +0000

    アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @user = User.all
   end
@@ -24,9 +26,18 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
+
   def user_params
     params.require(:user).permit(:image,:name,:introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to users_path
+    end
   end
 
 end
